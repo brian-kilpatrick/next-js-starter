7a10a1c8698d686682851f6367be620821183e30
Brian O. Kilpatrick <bkilpatrick@judge.com>
2018-02-22 00:19:39 -0500

    Preliminary login page.

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,33 @@
+import React from 'react';
 import Layout from '../components/layout';
-import { Modal, Header, Button, } from 'semantic-ui-react'
+import { Card, Form, Transition } from 'semantic-ui-react'
 import '../styles/index.scss';
 
-export default () => (
+const form = (
+    <Form className="sign-in-form" action='/sign-in' method="POST">
+      <Form.Input icon="user" iconPosition='left' name="email" placeholder="Email Address"/>
+      <Form.Input icon="lock" iconPosition='left' name="password" placeholder="Password" type="password" />
+      <Form.Group>
+        <Form.Button color="blue">Submit</Form.Button>
+        <Form.Button>Forgot Password?</Form.Button>
+      </Form.Group>
+    </Form>
+);
+
+const Login = () => (
   <Layout>
     <div className="app-wrapper">
-      <Modal trigger={<Button>Show Modal</Button>}>
-        <Modal.Header>Select a Photo</Modal.Header>
-        <Modal.Content image>
-          <Modal.Description>
-            <Header>Default Profile Image</Header>
-            <p>We've found the following gravatar image associated with your e-mail address.</p>
-            <p>Is it okay to use this photo?</p>
-          </Modal.Description>
-        </Modal.Content>
-      </Modal>
+      <Transition transitionOnMount={true} animation="fade" duration={1500}>
+        <h1 className="logo">::SetList</h1>
+      </Transition>
+      <Transition transitionOnMount={true} animation="fade" duration={1500}>
+          <Card className="sign-in-card"
+                raised={true} header="Sign-In"
+                description={form}
+                extra="Sign into Setlist"/>
+      </Transition>
     </div>
   </Layout>
-)
+);
+
+export default Login;
--- server.js
+++ server.js
@@ -6,9 +6,19 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
 app.prepare().then(() => {
   const server = express();
 
+  server.use(cookieParser());
+  server.use(bodyParser.urlencoded({ extended: true }));
+  server.use(bodyParser.json());
+
+  server.post('/sign-in', (req, res) => {
+    res.json(req.body)
+  });
 
   server.get('/api/example', (req, res) => res.json({ example: true }));
 
